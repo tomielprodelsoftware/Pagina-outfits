ef2b5b648b3192e5bcd7b7d025eae60a571f8b34
iñaki <gongorainaki@gmail.com>
2024-07-27 12:40:29 -0300

    Orden y app.py

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/')
+def index():
+    image_files = os.listdir(app.config['UPLOAD_FOLDER'])
+    image_files = [f for f in image_files if allowed_file(f)]
+    return render_template('index.html', images=image_files)
+
+@app.route('/outfits')
+def outfits():
+    return render_template('/templates/outfits.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+
+if __name__ == '__main__':
+    app.run(debug=True)
