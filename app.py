bedcd575d1ddecbe214debe2403e3369ff0ab859
tomielprodelsoftware <t.fuentes@alumno.etec.um.edu.ar>
2024-07-27 14:00:58 -0300

    codigo arreglado

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from werkzeug.utils import secure_filename
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = './static/uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+@app.route('/')
+def index():
+    image_files = os.listdir(app.config['UPLOAD_FOLDER'])
+    image_files = [f for f in image_files if allowed_file(f)]
+    return render_template('index.html', images=image_files)
+
+@app.route('/outfits')
+def outfits():
+    return render_template('outfits.html')
+
+@app.route('/upload', methods=['GET', 'POST'])
+def upload_file():
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            return redirect(url_for('index'))
+    return render_template('upload.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
